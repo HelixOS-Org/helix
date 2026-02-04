ae1724d75b5653760d75f897f1ababf0b1cdf3b4
Helix <helix.dev654165@proton.me>
2026-02-04 14:10:31 -0500

    style: conditionally enable tests based on std feature

--- boot/uefi/src/power/mod.rs
+++ boot/uefi/src/power/mod.rs
@@ -1144,7 +1144,7 @@ impl fmt::Display for AcpiError {
 // TESTS
 // =============================================================================
 
-#[cfg(test)]
+#[cfg(all(test, feature = "std"))]
 mod tests {
     use super::*;
 
