6dfb66a13dad67f7971f90672d25a0d230319165
Helix <helix.dev654165@proton.me>
2026-02-04 14:11:22 -0500

    style: conditionally enable tests based on std feature

--- boot/uefi/src/protocols/mod.rs
+++ boot/uefi/src/protocols/mod.rs
@@ -555,7 +555,7 @@ extern crate alloc;
 // TESTS
 // =============================================================================
 
-#[cfg(test)]
+#[cfg(all(test, feature = "std"))]
 mod tests {
     use super::*;
 
