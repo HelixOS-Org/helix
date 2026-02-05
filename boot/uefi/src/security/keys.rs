a44fde7904c747b2442ca68bba93d5c1a907862e
Helix <helix.dev654165@proton.me>
2026-02-05 13:48:09 -0500

    style: improve readability of conditional logic in is_ca function

--- boot/uefi/src/security/keys.rs
+++ boot/uefi/src/security/keys.rs
@@ -539,10 +539,7 @@ impl X509Certificate {
         for ext in &self.extensions {
             if ext.oid == BASIC_CONSTRAINTS_OID {
                 // Parse SEQUENCE { BOOLEAN, INTEGER OPTIONAL }
-                if ext.value.len() >= 5
-                    && ext.value[0] == 0x30
-                    && ext.value[2] == 0x01
-                {
+                if ext.value.len() >= 5 && ext.value[0] == 0x30 && ext.value[2] == 0x01 {
                     return ext.value[4] == 0xff;
                 }
             }
