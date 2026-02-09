3ceca403528defdf58e339ef859c273e58bc3bd6
Helix <helix.dev654165@proton.me>
2026-02-09 17:17:01 -0500

    style: add inline attribute to attack method for optimization

--- subsystems/nexus/src/adversarial/fgsm.rs
+++ subsystems/nexus/src/adversarial/fgsm.rs
@@ -23,6 +23,7 @@ impl FGSM {
     }
 
     /// Generate adversarial example
+    #[inline]
     pub fn attack(&self, input: &[f64], gradient: &[f64]) -> Perturbation {
         let mut perturbation = Perturbation::new(input.len(), self.pert_type, self.epsilon);
 
