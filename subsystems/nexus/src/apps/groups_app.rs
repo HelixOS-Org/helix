4e815c860e63dd1dbb54c1818e6a652e6ee6cc7a
Helix <helix.dev654165@proton.me>
2026-02-09 17:30:55 -0500

    style: add inline attributes for optimization in groups_app.rs

--- subsystems/nexus/src/apps/groups_app.rs
+++ subsystems/nexus/src/apps/groups_app.rs
@@ -43,6 +43,7 @@ impl GroupsRecord {
 
 /// Groups app stats
 #[derive(Debug, Clone)]
+#[repr(align(64))]
 pub struct GroupsAppStats {
     pub total_ops: u64,
     pub getgroups: u64,
@@ -68,6 +69,7 @@ impl AppGroups {
         }
     }
 
+    #[inline]
     pub fn record(&mut self, rec: &GroupsRecord) {
         self.stats.total_ops += 1;
         match rec.op {
