17e5f3496f1c893782e42510feecf1be7d55e172
Helix <helix.dev654165@proton.me>
2026-02-10 12:56:27 -0500

    style: remove unused String import to clean up code

--- subsystems/nexus/src/apps/transcend/optimal.rs
+++ subsystems/nexus/src/apps/transcend/optimal.rs
@@ -9,7 +9,6 @@
 extern crate alloc;
 
 use alloc::collections::BTreeMap;
-use alloc::string::String;
 use alloc::vec::Vec;
 
 // ---------------------------------------------------------------------------
