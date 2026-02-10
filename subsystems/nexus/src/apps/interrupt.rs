5f186ee2ee76a257f981ab40a2515c61eb1db513
Helix <helix.dev654165@proton.me>
2026-02-10 11:44:25 -0500

    style: remove unused Vec import in interrupt.rs

--- subsystems/nexus/src/apps/interrupt.rs
+++ subsystems/nexus/src/apps/interrupt.rs
@@ -11,7 +11,6 @@ extern crate alloc;
 
 use crate::fast::array_map::ArrayMap;
 use alloc::collections::BTreeMap;
-use alloc::vec::Vec;
 
 // ============================================================================
 // IRQ TYPES
