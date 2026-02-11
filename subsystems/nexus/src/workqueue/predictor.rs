125299bb1a2ffd232fa17fb208ea609d51163efd
Helix <helix.dev654165@proton.me>
2026-02-11 04:12:51 -0500

    style: replace pop_front with remove for clarity in record_sample method

--- subsystems/nexus/src/workqueue/predictor.rs
+++ subsystems/nexus/src/workqueue/predictor.rs
@@ -80,7 +80,7 @@ impl QueueDepthPredictor {
 
         // Add sample
         if self.samples.len() >= self.max_samples {
-            self.samples.pop_front();
+            self.samples.remove(0);
         }
         self.samples.push_back(sample);
     }
