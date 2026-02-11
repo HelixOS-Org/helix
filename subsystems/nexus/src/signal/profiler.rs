8904d7ec62e724d3ec7762ac179d5dc4a82cd767
Helix <helix.dev654165@proton.me>
2026-02-11 04:09:23 -0500

    style: optimize sample removal in HandlerProfiler by using remove(0) instead of pop_front()

--- subsystems/nexus/src/signal/profiler.rs
+++ subsystems/nexus/src/signal/profiler.rs
@@ -175,7 +175,7 @@ impl HandlerProfiler {
             timestamp,
         };
         if self.samples.len() >= self.max_samples {
-            self.samples.pop_front();
+            self.samples.remove(0);
         }
         self.samples.push_back(sample);
     }
